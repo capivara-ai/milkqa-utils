4debf7006dc53bb7538ad9561c9411cbc38fb9c7
Lyssa Scherer <lyssascherer@Lyssas-MacBook-Pro.local>
2019-08-12 21:09:18 +0200

    Add type hintng to method declaration

--- milkqa_utils/preprocessing.py
+++ milkqa_utils/preprocessing.py
@@ -4,48 +4,48 @@ import re
 import string
 
 
-def tokenize(input_string):
+def tokenize(input_string: str) -> list:
     """Tokenizes a string using ``nltk.word_tokenizer`` and returns the list of tokens.
 
     Args:
-        input_string (string): The input string.
-    
+        input_string: The input string.
+
     Returns:
         A list of tokens.
     """
     return nltk.word_tokenize(input_string)
 
 
-def has_alphanum(token):
+def has_alphanum(token: str) -> bool:
     """Checks whether a token contains alphanumeric characters.
 
     Args:
-        token (string): The input token.
-    
+        token: The input token.
+
     Returns:
         True if ``token`` contains at least one alphanumeric character, False otherwise.
     """
     return any(re.findall(r"[a-zA-Z]|[0-9]", token))
 
 
-def remove_punctuation(token_list):
+def remove_punctuation(token_list: iterable) -> filter:
     """Removes punctuation-only tokens from ``token_list``.
 
     Args:
-        token_list (iterable): A list of tokens, tokenized by ``tokenize()``.
-    
+        token_list: A list of tokens, tokenized by ``tokenize()``.
+
     Returns:
         A filter containing only the non-punctuation token from ``token_list``.
     """
     return filter(has_alphanum, token_list)
 
 
-def remove_stopwords(token_list):
+def remove_stopwords(token_list: iterable) -> filter:
     """Removes stopwords from ``token_list`` according to the list given by nltk.
 
     Args:
-        token_list (iterable): A list of tokens, tokenized by ``tokenize()``.
-    
+        token_list: A list of tokens, tokenized by ``tokenize()``.
+
     Returns:
         A filter containing only non-stopword tokens from ``token_list``.
     """
